2b364934c6067c916328bb56ff57c9a88386423f
siraegit <siraegit@gmail.com>
2025-05-27 19:47:04 +0900

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -21,11 +21,12 @@ st.subheader(data.get("title", "제목 없음"))
 
 # 미디어 타입에 따라 사진 또는 영상 출력
 if data.get("media_type") == "image":
-    st.image(data.get("url"), use_column_width=True)
+    st.image(data.get("url"), use_container_width=True)
 elif data.get("media_type") == "video":
     st.video(data.get("url"))
 else:
     st.warning("지원하지 않는 미디어 형식입니다.")
+
 
 # 설명 출력
 st.markdown(f"**설명:** {data.get('explanation', '설명이 없습니다.')}")
